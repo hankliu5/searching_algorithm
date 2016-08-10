175eb734709d4382107a6c8d1c023674ca5e1bac
hankliu5 <liu19901124@gmail.com>
2016-08-09 17:27:24 -0700

    changed into an module

--- SearchAlgorithms.rb
+++ SearchAlgorithms.rb
@@ -1,33 +1,29 @@
-# Linear Search
-def linearSearch(arr, target)
-  i = 0
-  loop do
-    break if i >= arr.length
-    return i if target == arr[i]
-    i += 1
-  end
-  return 0
-end
-
-# Binary Search
-def binarySearch(arr, target)
-  i = 0
-  j = arr.length - 1
-  while i <= j
-    m = (i + j) / 2
-    if target == arr[m]
-      return m
-    elsif target > arr[m]
-      i = m + 1
-    elsif target < arr[m]
-      j = m - 1
+module SearchAlgorithms
+  # Linear Search
+  def SearchAlgorithms.linearSearch(arr, target)
+    i = 0
+    loop do
+      break if i >= arr.length
+      return i if target == arr[i]
+      i += 1
     end
+    return 0
   end
-  return 0
+
+  # Binary Search
+  def SearchAlgorithms.binarySearch(arr, target)
+    i = 0
+    j = arr.length - 1
+    while i <= j
+      m = (i + j) / 2
+      if target == arr[m]
+        return m
+      elsif target > arr[m]
+        i = m + 1
+      elsif target < arr[m]
+        j = m - 1
+      end
+    end
+    return 0
+  end
 end
-
-array = Array(1..10000000)
-target = rand 1..10000000
-
-puts "linear: #{linearSearch(array, target)}"
-puts "binary: #{binarySearch(array, target)}"
